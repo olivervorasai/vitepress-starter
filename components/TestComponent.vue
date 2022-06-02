6ec0edd66c0d0ff2010ba1d717d30b1699975b76
Oliver Vorasai <olivervorasai@gmail.com>
2022-06-01 22:38:46 -0700

    initial commit

---
+++ components/TestComponent.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const name = "Test Component";
+</script>
+
+<template>
+    <div>Hello my name is {{ name }}</div>
+</template>
+
+<style scoped>
+div {
+    border: 2px solid black;
+    margin: 2rem;
+    padding: 0.5rem;
+}
+</style>
---
+++ docs/index.md
@@ -0,0 +1,9 @@
+# Hello VitePress
+
+<!-- This will work if 'lang="ts"' is removed -->
+<script setup lang="ts">
+import TestComponent from '../components/TestComponent.vue'
+</script>
+
+My test component should be visible below:
+<TestComponent />
